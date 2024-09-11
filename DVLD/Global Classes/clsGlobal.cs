6d67623324a7e96b48380f46b0ce31de83a57706
Youssif <youssifahmed12397@gmail.com>
2024-09-11 13:35:57 +0300

    first commit

---
+++ DVLD/Global Classes/clsGlobal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Buisness;
+using Microsoft.Win32;
+
+
+namespace DVLD.Classes
+{
+    internal static  class clsGlobal
+    {
+        public static clsUser CurrentUser;
+        private const string registryPath = @"SOFTWARE\DVLD\Credentials";
+        public static bool RememberUsernameAndPassword(string Username, string Password)
+        {
+            try
+            {
+               
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(registryPath);
+
+                
+                if (string.IsNullOrEmpty(Username))
+                {
+                    Registry.CurrentUser.DeleteSubKey(registryPath, false);
+                    return true;
+                }
+
+                
+                key.SetValue("Username", Username);
+                key.SetValue("Password", Password);
+
+                key.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+                return false;
+            }
+        }
+
+       
+        public static bool GetStoredCredential(ref string Username, ref string Password)
+        {
+            try
+            {
+                
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(registryPath);
+
+                
+                if (key != null)
+                {
+                   
+                    Username = key.GetValue("Username")?.ToString() ?? "";
+                    Password = key.GetValue("Password")?.ToString() ?? "";
+
+                    key.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ DVLD_DataAccess/ApplicationType.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DVLD_DataAccess.clsCountryData;
+using System.Net;
+using System.Security.Policy;
+using System.Configuration;
+
+namespace DVLD_DataAccess
+{
+    public class clsApplicationTypeData
+    {
+
+        public static bool GetApplicationTypeInfoByID(int ApplicationTypeID,
+            ref string ApplicationTypeTitle, ref float ApplicationFees)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM ApplicationTypes WHERE ApplicationTypeID = @ApplicationTypeID";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ApplicationTypeID", ApplicationTypeID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+
+                                // The record was found
+                                isFound = true;
+
+                                ApplicationTypeTitle = (string)reader["ApplicationTypeTitle"];
+                                ApplicationFees = Convert.ToSingle(reader["ApplicationFees"]);
+
+
+
+
+
+                            }
+                            else
+                            {
+                                // The record was not found
+                                isFound = false;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                
+                isFound = false;
+            }
+
+
+            return isFound;
+        }
+
+        public static DataTable GetAllApplicationTypes()
+        {
+
+            DataTable dt = new DataTable();
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM ApplicationTypes order by ApplicationTypeTitle";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+
+                            {
+                                dt.Load(reader);
+                            }
+
+
+
+                        }
+                    }
+
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                
+            }
+
+            return dt;
+
+        }
+
+        public static int AddNewApplicationType(string Title, float Fees)
+        {
+            int ApplicationTypeID = -1;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
+                {
+                    connection.Open();
+                    string query = @"Insert Into ApplicationTypes (ApplicationTypeTitle,ApplicationFees)
+                            Values (@Title,@Fees)
+                            
+                            SELECT SCOPE_IDENTITY();";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+                        command.Parameters.AddWithValue("@ApplicationTypeTitle", Title);
+                        command.Parameters.AddWithValue("@ApplicationFees", Fees);
+
+                        object result = command.ExecuteScalar();
+
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            ApplicationTypeID = insertedID;
+                        }
+                    }
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                
+
+            }
+
+
+
+            return ApplicationTypeID;
+
+        }
+
+        public static bool UpdateApplicationType(int ApplicationTypeID, string Title, float Fees)
+        {
+
+            int rowsAffected = 0;
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = @"Update  ApplicationTypes  
+                            set ApplicationTypeTitle = @Title,
+                                ApplicationFees = @Fees
+                                where ApplicationTypeID = @ApplicationTypeID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+                        command.Parameters.AddWithValue("@ApplicationTypeID", ApplicationTypeID);
+                        command.Parameters.AddWithValue("@Title", Title);
+                        command.Parameters.AddWithValue("@Fees", Fees);
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                
+                return false;
+            }
+
+
+
+            return (rowsAffected > 0);
+        }
+
+    }
+}
